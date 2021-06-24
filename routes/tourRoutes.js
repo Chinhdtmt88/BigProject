02e50b8dcde5b862c9c6217b68392b97906bb2a3
Nguyen Van Chinh <72721577+Chinhdtmt88@users.noreply.github.com>
2021-06-24 17:42:37 +0700

    24/6

---
+++ routes/tourRoutes.js
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+const express = require('express');
+
+const router = express.Router();
+
+const tourController = require('../controllers/tourController');
+
+//router.param('id', tourController.checkID);
+//Create a checkBody middleware
+//Check if body contains the name and price property
+//If not, send back 400 (bad request)
+//Add it to the post handler stack
+
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
