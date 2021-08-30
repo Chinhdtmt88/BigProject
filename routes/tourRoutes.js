416a8cb3c6e3e61a5cd3b4db9621499a1469f728
Nguyen Van Chinh <72721577+Chinhdtmt88@users.noreply.github.com>
2021-08-30 08:27:15 +0700

    coordinates distances

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -36,6 +36,8 @@ router
 // /tour-distance?distance=233&center=-40,45&unit=mi
 // /tours-distance/233/center/-40,45/unit
 
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router
   .route('/')
   .get(tourController.getAllTours)
